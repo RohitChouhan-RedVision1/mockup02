856a4f620cacca917cc2506021f348f43d37163f
RohitChouhan-RedVision1 <Rohit.Chouhan@redvisionglobal.com>
2025-04-16 18:32:59 +0530

    changes done

--- app/(frontoned)/forget-password/page.jsx
+++ app/(frontoned)/forget-password/page.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/next-auth";
-import { redirect } from "next/navigation";
-import ForgetPassword from "@/components/auth/ForgetPassword";
+// import React from "react";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "@/lib/next-auth";
+// import { redirect } from "next/navigation";
+// import ForgetPassword from "@/components/auth/ForgetPassword";
 
-const SignIn = async () => {
-  return (
-    <>
-      {/* source:https://codepen.io/owaiswiz/pen/jOPvEPB */}
-      <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center py-20 px-12">
-        <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-          <ForgetPassword />
-          <div className="flex-1 bg-indigo-100 text-center hidden lg:flex " style={{
-                backgroundImage:
-                  'url("/LOGIN 9.jpg")',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: 'cover', // or 'contain' based on your needs
-                  backgroundPosition: 'center' // Adjust as needed
-              }}>
-          </div>
-        </div>
-      </div>
+// const SignIn = async () => {
+//   return (
+//     <>
+//       {/* source:https://codepen.io/owaiswiz/pen/jOPvEPB */}
+//       <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center py-20 px-12">
+//         <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+//           <ForgetPassword />
+//           <div className="flex-1 bg-indigo-100 text-center hidden lg:flex " style={{
+//                 backgroundImage:
+//                   'url("/LOGIN 9.jpg")',
+//                   backgroundRepeat: 'no-repeat',
+//                   backgroundSize: 'cover', // or 'contain' based on your needs
+//                   backgroundPosition: 'center' // Adjust as needed
+//               }}>
+//           </div>
+//         </div>
+//       </div>
      
-    </>
-  );
-};
+//     </>
+//   );
+// };
 
-export default SignIn;
+// export default SignIn;
--- app/(frontoned)/login/page.jsx
+++ app/(frontoned)/login/page.jsx
@@ -1,28 +1,28 @@
-import React from "react";
+// import React from "react";
 
-import Signin from "@/components/auth/Signin";
+// import Signin from "@/components/auth/Signin";
 
-const SignIn = async () => {
-  return (
-    <>
-      {/* source:https://codepen.io/owaiswiz/pen/jOPvEPB */}
-      <div className="min-h-[500px] bg-gray-100 text-gray-900 flex justify-center ">
-        <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-        <div className="flex-1 bg-indigo-100 text-center hidden lg:flex " style={{
-                backgroundImage:
-                  'url("/LOGIN 9.jpg")',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: 'cover', // or 'contain' based on your needs
-                  backgroundPosition: 'center' // Adjust as needed
-              }}>
-          </div>
-          <Signin />
+// const SignIn = async () => {
+//   return (
+//     <>
+//       {/* source:https://codepen.io/owaiswiz/pen/jOPvEPB */}
+//       <div className="min-h-[500px] bg-gray-100 text-gray-900 flex justify-center ">
+//         <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+//         <div className="flex-1 bg-indigo-100 text-center hidden lg:flex " style={{
+//                 backgroundImage:
+//                   'url("/LOGIN 9.jpg")',
+//                   backgroundRepeat: 'no-repeat',
+//                   backgroundSize: 'cover', // or 'contain' based on your needs
+//                   backgroundPosition: 'center' // Adjust as needed
+//               }}>
+//           </div>
+//           <Signin />
           
-        </div>
-      </div>
+//         </div>
+//       </div>
      
-    </>
-  );
-};
+//     </>
+//   );
+// };
 
-export default SignIn;
+// export default SignIn;
